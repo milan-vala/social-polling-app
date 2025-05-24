d12ef5413a9219a7728931186f9d2909c49beea3
Milan Vala <milanvala31@gmail.com>
2025-05-25 01:35:06 +0530

    refactor: clean up TypeScript type definitions
    - Add required Poll, Option, Vote, and User interfaces
    - Create helper Types alias to reduce duplication
    - Implement DRY principles for cleaner type definitions
    - Ensure strict TypeScript compliance for challenge requirements

--- src/types/database.ts
+++ src/types/database.ts
@@ -76,3 +76,23 @@ export interface Database {
     };
   };
 }
+
+type Tables = Database["public"]["Tables"];
+
+export type Poll = Tables["polls"]["Row"];
+export type PollInsert = Tables["polls"]["Insert"];
+export type PollUpdate = Tables["polls"]["Update"];
+
+export type Option = Tables["poll_options"]["Row"];
+export type OptionInsert = Tables["poll_options"]["Insert"];
+export type OptionUpdate = Tables["poll_options"]["Update"];
+
+export type Vote = Tables["votes"]["Row"];
+export type VoteInsert = Tables["votes"]["Insert"];
+export type VoteUpdate = Tables["votes"]["Update"];
+
+export interface User {
+  id: string;
+  email: string;
+  created_at: string;
+}
